f157d03d53124330b3913cfab7b84c99f7f8da24
junya takaesu <junya.takaesu@gmail.com>
2021-08-09 21:13:39 +0900

    Create ConsoleLog Component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ConsoleLog from "./Components/ConsoleLog";
+
+function App() {
+  return (
+    <div className="container">
+      <ConsoleLog />
+    </div>
+  );
+}
+
+export default App;
